f867b55f5f8bfc379a9bd6e8af7f60f9026f8022
Kelly Vieira <kellyvieira09@hotmail.com>
2021-12-18 22:08:46 -0300

    semana5

---
+++ database.js
@@ -0,0 +1,30 @@
+//cria o array de objetos estudantes 
+const livros = [
+    {
+      nome: 'incluir'
+    },
+    {
+      nome: 'incluir'
+    },
+    {
+      nome: 'incluir'
+    },
+    {
+      nome: 'incluir'
+    },
+    {
+      nome: 'incluir'
+    },
+    {
+      nome: 'incluir'
+    },
+    {
+      nome: 'incluir'
+    },
+    {
+      nome: 'incluir'
+    },
+]
+
+//exporta o array de objetos estudantes
+module.exports = livros
